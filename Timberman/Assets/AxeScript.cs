22414037323d471d3cb659187b46553192df103c
rkohub <ryangithub@gmail.com>
2022-04-11 22:39:24 -0400

    Copied The Unity Project
    Copied The Unity Project into the github repo.

---
+++ Timberman/Assets/AxeScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AxeScript : MonoBehaviour{
+    // Start is called before the first frame update
+    public GameObject Tree;
+    private TreeBuilder TreeBuilder;
+    public bool gameOver;
+
+    public int score;
+    public float elapsedTime;
+
+    public Text scoreText;
+    public Text timerText;
+
+    //TODO
+
+    void Start(){
+        // System.out.println("TESTING");
+        // Debug.Log("TESTING");
+        this.gameObject.transform.position = new Vector3 (1,0,0);
+        TreeBuilder = Tree.GetComponent<TreeBuilder>();
+        // Debug.Log(TreeBuilder.testNum);
+        gameOver = false;
+        score = 0;
+        elapsedTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(!gameOver){
+            if (Input.GetKeyDown(KeyCode.F)||Input.GetKeyDown(KeyCode.D)){
+                // Debug.Log("F");
+                chopLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.J)||Input.GetKeyDown(KeyCode.K)){
+                // Debug.Log("J");
+                chopRight();
+            }
+            if(TreeBuilder.tree[0] != 0 && this.gameObject.transform.position.x == Mathf.Sign(TreeBuilder.tree[0])){
+                Debug.Log("GAME OVER");
+                gameOver = true;
+            }else{
+                // Debug.Log("ALIVE");
+            }
+            if(score != 0){
+                elapsedTime += Time.deltaTime;
+            }
+
+            scoreText.text = "Score: " + score;
+            if(score < 100){
+                timerText.text = "Time: " + Mathf.Round(elapsedTime * 100) / 100.0f;
+            }
+            // timerText.text = "Time: " + Mathf.Round(elapsedTime * 100) / 100.0f;
+        }
+    }
+
+    void chopLeft(){
+        if(TreeBuilder.tree[0] != 0 && Mathf.Sign(TreeBuilder.tree[0]) == -1){
+            gameOver = true;
+            Debug.Log("GAME OVER");
+            this.gameObject.transform.position = new Vector3 (-1,0,0);
+            return;
+        }
+        // Debug.Log("Left");
+        this.gameObject.transform.position = new Vector3 (-1,0,0);
+        TreeBuilder.shiftTree();
+        score++;
+    }
+
+    void chopRight(){
+        if(TreeBuilder.tree[0] != 0 && Mathf.Sign(TreeBuilder.tree[0]) == 1){
+            gameOver = true;
+            Debug.Log("GAME OVER");
+            this.gameObject.transform.position = new Vector3 (1,0,0);
+            return;
+        }
+        // Debug.Log("Right");
+        this.gameObject.transform.position = new Vector3 (1,0,0);
+        TreeBuilder.shiftTree();
+        score++;
+    }
+}
---
+++ Timberman/Assets/TreeBuilder.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBuilder : MonoBehaviour{
+    // Start is called before the first frame update
+
+    public GameObject Trunk;
+    public GameObject Branch;
+
+    public int newestL;
+    public int newestR;
+
+    // public int testNum = 3423423;
+
+    public int[] tree = new int[10];
+
+    void Start(){
+        newestL = 0;
+        newestR = 0;
+        tree[0] = 0;
+        tree[1] = 0;
+        tree[2] = 0;
+        tree[3] = -1;
+        for(int i=4; i<10; i++){
+
+            newestR--;
+            newestL--;
+            if(tree[i-1] != 0){
+                tree[i] = 0;
+            }else if(newestL < 2){
+                tree[i] = -1;
+                newestL = 9;
+            }else if(newestR < 2){
+                tree[i] = 1;
+                newestR = 9;
+            }else{
+                tree[i] = Random.Range(-3,4);
+                if(tree[i]<0){
+                    newestL = 9;
+                }
+                if(tree[i]>0){
+                    newestR = 9;
+                }
+            }
+        }
+        /*
+        for(int i=0; i<10; i++){
+            Debug.Log(tree[i]);
+        }
+        //*/
+        drawTree();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+        if (Input.GetKeyDown(KeyCode.Q)){
+            shiftTree();
+        }
+        // Debug.Log(this.gameObject.transform.childCount > 0);
+
+        //Debug.Log("NewestL: " + newestL);
+        //Debug.Log("NewestR: " + newestR);
+    }
+
+    void drawTree(){
+        // Debug.Log("DRAW");
+        // while(this.gameObject.transform.childCount > 0){
+        if(this.gameObject.transform.childCount > 0){
+            // Debug.Log("LOOP");
+            for(int i=0; i<this.gameObject.transform.childCount; i++){
+                // Debug.Log(this.gameObject.transform.childCount > 0);
+                // this.gameObject.transform.GetChild(0).gameObject.destroy;
+                // Debug.Log(this.gameObject.transform.GetChild(i).gameObject);
+                Destroy(this.gameObject.transform.GetChild(i).gameObject);
+            }
+        }
+        //*
+        for(int i=0; i < 10; i++){
+            Instantiate(Trunk,new Vector3 (0,i,0), Quaternion.identity, this.gameObject.transform);
+            if(tree[i] != 0){
+                Instantiate(Branch, new Vector3 (Mathf.Sign(tree[i]),i,0), Quaternion.identity, this.gameObject.transform);
+            }
+            
+        }
+        //*/
+    }
+
+    public void shiftTree(){
+        // Debug.Log("SHIFT");
+        for(int i=0; i < 9; i++){
+            tree[i] = tree[i+1];
+        }
+        newestR--;
+        newestL--;
+        if(tree[8] != 0){
+                tree[9] = 0;
+        }else if(newestL < 2){
+            tree[9] = -1;
+            newestL = 9;
+        }else if(newestR < 2){
+            tree[9] = 1;
+            newestR = 9;
+        }else{
+            tree[9] = Random.Range(-3,4);
+            if(tree[9]<0){
+                newestL = 9;
+            }
+            if(tree[9]>0){
+                newestR = 9;
+            }
+        }
+        drawTree();
+    }
+}
